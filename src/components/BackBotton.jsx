354fee7f8e27ad03f34750c53ac6810f1bde15c8
GusevADresume <resumeGusevAD@gmail.com>
2023-11-30 19:17:23 +0300

    change: url3

--- src/components/BackBotton.jsx
+++ src/components/BackBotton.jsx
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom';
 export default function BackBotton() {
   return (
     <div className='BackBotton'>
-      <Link className='BackUrl' to='/'>Home</Link>
+      <Link className='BackUrl' to='/My_portfolio/'>Home</Link>
     </div>
   )
 }
