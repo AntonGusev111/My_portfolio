12ff1567691ca88c047cd727ef390c61b82ece2b
GusevADresume <resumeGusevAD@gmail.com>
2023-12-16 01:15:13 +0300

    fix

--- src/pages/WorkPage.jsx
+++ src/pages/WorkPage.jsx
@@ -1,14 +1,16 @@
 import React from "react";
 import { useParams } from "react-router-dom";
-import { useRef, useEffect } from "react";
+import Loader from "../components/Loader";
+import { useRef, useState } from "react";
 import BackBotton from "../components/BackBotton";
 import data from "../assets/json/full.json";
 import Slider from "react-slick";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 
+
 export default function WorkPage() {
-  const imgRef = useRef();
+  const [load1, setLoader] = useState(false);
   const param = useParams();
   const info = data[param.id];
   const imgs = Object.keys(info.content).map((element) => {
@@ -20,8 +22,15 @@ export default function WorkPage() {
     speed: 500,
     slidesToShow: 1,
     slidesToScroll: 1,
-    className:"mySlider",
-    arrows:false,
+    className: "mySlider",
+    arrows: false,
+  };
+
+  
+  const load = (e) => {
+    if (e) {
+      setLoader(true);
+    }
   };
 
   return (
@@ -31,20 +40,22 @@ export default function WorkPage() {
       <div className="WorkPageDescription Font27 ColorWhite">
         {Object.keys(info.desc)}:{Object.values(info.desc)}
       </div>
+      <div className="AnimatedPullWraper">
+        <div className="AnimatedPull">
+           PULL
+        </div>
+      </div>
+
       <div className="contentWraper">
         <div className="MysliderWraper">
-          <Slider
-            onSwipeStart={() => {
-              console.log("awd");
-            }}
-            {...carouselSettings}
-          >
+          {load1? "":<Loader/>}
+          <Slider {...carouselSettings}>
             {Object.keys(info.content).map((element, index) => {
-              console.log()
               return (
                 <>
                   <img
                     className="sliderImg"
+                    onLoad={(e) => load(e)}
                     key={index}
                     src={element}
                     alt="slide-1"
